de89e467054fd2b27ebbb7b8d16aff59c1763c1e
Raim Ant <antraim@outlook.com>
2023-06-21 11:32:18 +0300

    .

--- ZXing.Net.Mobile.Forms/ZXingBarcodeImageView.shared.cs
+++ ZXing.Net.Mobile.Forms/ZXingBarcodeImageView.shared.cs
@@ -5,42 +5,52 @@ using ZXing.Common;
 namespace ZXing.Net.Mobile.Forms
 {
     public class ZXingBarcodeImageView : Image
-	{
-		public ZXingBarcodeImageView() : base()
+    {
+        #region BindableProperties
+
+        public static readonly BindableProperty BarcodeFormatProperty =
+            BindableProperty.Create(nameof(BarcodeFormat), typeof(BarcodeFormat), typeof(ZXingBarcodeImageView),
+                defaultValue: BarcodeFormat.QR_CODE,
+                defaultBindingMode: BindingMode.TwoWay);
+
+        public static readonly BindableProperty BarcodeValueProperty =
+            BindableProperty.Create(nameof(BarcodeValue), typeof(string), typeof(ZXingBarcodeImageView),
+                defaultValue: string.Empty,
+                defaultBindingMode: BindingMode.TwoWay);
+
+
+        public static readonly BindableProperty BarcodeOptionsProperty =
+            BindableProperty.Create(nameof(BarcodeOptions), typeof(EncodingOptions), typeof(ZXingBarcodeImageView),
+                defaultValue: new EncodingOptions(),
+                defaultBindingMode: BindingMode.TwoWay);
+
+        #endregion
+
+        #region Properies
+
+        public BarcodeFormat BarcodeFormat
+        {
+            get => (BarcodeFormat)GetValue(BarcodeFormatProperty);
+            set => SetValue(BarcodeFormatProperty, value);
+        }
+
+        public string BarcodeValue
+        {
+            get => (string)GetValue(BarcodeValueProperty);
+            set => SetValue(BarcodeValueProperty, value);
+        }
+
+        public EncodingOptions BarcodeOptions
+        {
+            get => (EncodingOptions)GetValue(BarcodeOptionsProperty);
+            set => SetValue(BarcodeOptionsProperty, value);
+        }
+
+        #endregion
+
+        public ZXingBarcodeImageView() : base()
 		{
-		}
 
-		public static readonly BindableProperty BarcodeFormatProperty =
-			BindableProperty.Create(nameof(BarcodeFormat), typeof(BarcodeFormat), typeof(ZXingBarcodeImageView),
-				defaultValue: BarcodeFormat.QR_CODE,
-				defaultBindingMode: BindingMode.TwoWay);
-
-		public BarcodeFormat BarcodeFormat
-		{
-			get => (BarcodeFormat)GetValue(BarcodeFormatProperty);
-			set => SetValue(BarcodeFormatProperty, value);
-		}
-
-		public static readonly BindableProperty BarcodeValueProperty =
-			BindableProperty.Create(nameof(BarcodeValue), typeof(string), typeof(ZXingBarcodeImageView),
-				defaultValue: string.Empty,
-				defaultBindingMode: BindingMode.TwoWay);
-
-		public string BarcodeValue
-		{
-			get => (string)GetValue(BarcodeValueProperty);
-			set => SetValue(BarcodeValueProperty, value);
-		}
-
-		public static readonly BindableProperty BarcodeOptionsProperty =
-			BindableProperty.Create(nameof(BarcodeOptions), typeof(EncodingOptions), typeof(ZXingBarcodeImageView),
-				defaultValue: new EncodingOptions(),
-				defaultBindingMode: BindingMode.TwoWay);
-
-		public EncodingOptions BarcodeOptions
-		{
-			get => (EncodingOptions)GetValue(BarcodeOptionsProperty);
-			set => SetValue(BarcodeOptionsProperty, value);
 		}
 	}
 }
